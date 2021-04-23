bcdd2729d5a3ccb5134e73313c67fce448d6018e
cindy <ci.ri@web.de>
2021-04-23 13:54:32 +0200

    Spring Boot Application

---
+++ src/main/java/com/ciri/codingchallenge/CodingChallengeApplication.java
@@ -0,0 +1,14 @@
+package com.ciri.codingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class CodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingChallengeApplication.class, args);
+	}
+}
